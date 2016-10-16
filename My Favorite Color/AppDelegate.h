cd64732832414ef5bdd1ad8d1d748c5983c3deb6
mac <sabbirahmed@learniosfreecoms-mac.local>
2016-10-16 13:14:36 +0600

    Initial Commit

---
+++ My Favorite Color/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My Favorite Color
+//
+//  Created by Sabbir Ahmed on 10/16/16.
+//  Copyright © 2016 Sabbir Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
